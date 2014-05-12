6702638e1145a8c83f4e08b662c0cbc112e494e9
Petr Baros <pbaros@gmail.com>
2014-05-12 23:50:01 +0200

    Add paddles, ball and a little movement

---
+++ Pong/Assets/Scripts/MovingBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingBall : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody2D.AddForce(new Vector2(-300, 0));
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Pong/Assets/Scripts/MovingPaddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPaddle : MonoBehaviour
+{
+    public KeyCode up;
+    public KeyCode down;
+    public float speed = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            transform.position += new Vector3(0, speed, 0);
+        } else if (Input.GetKey(down))
+        {
+            transform.position += new Vector3(0, -speed, 0);
+        }
+    }
+
+}

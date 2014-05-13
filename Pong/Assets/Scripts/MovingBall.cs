66486dcd96c5a27f9e07ceae2ff28de139863ab2
Petr Baros <pbaros@gmail.com>
2014-05-13 23:16:23 +0200

    Pong: added walls and starting force ball

--- Pong/Assets/Scripts/MovingBall.cs
+++ Pong/Assets/Scripts/MovingBall.cs
@@ -3,15 +3,35 @@ using System.Collections;
 
 public class MovingBall : MonoBehaviour
 {
+    private float speed = 300;
+    private bool readyToStart;
+    private float startTime;
+
     // Use this for initialization
     void Start()
     {
-        rigidbody2D.AddForce(new Vector2(-300, 0));
+        StartBall(2);
     }
     
     // Update is called once per frame
     void Update()
     {
-    
+        if (readyToStart && (Time.time > startTime))
+            ForceBall();
+    }
+
+    void StartBall(float delay)
+    {
+        readyToStart = true;
+        startTime = Time.time + delay;
+    }
+
+    void ForceBall()
+    {
+        float angle = Mathf.Sign(Random.Range(-1, 1)) * Mathf.Deg2Rad * Random.Range(20, 45);
+        float x = Mathf.Sign(Random.Range(-1, 1)) * Mathf.Cos(angle) * speed;
+        float y = Mathf.Sin(angle) * speed;
+        rigidbody2D.AddForce(new Vector2(x, y));
+        readyToStart = false;
     }
 }
---
+++ Pong/Assets/Scripts/PlacingWall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacingWall : MonoBehaviour {
+
+    public bool isUp;
+
+	// Use this for initialization
+	void Start () {
+        Vector3 worldSize = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        transform.position = new Vector3(transform.position.x, ((isUp) ? 1 : -1) * (worldSize.y - renderer.bounds.extents.y / 2), 0);
+        transform.localScale = new Vector3(worldSize.x / renderer.bounds.extents.x, 1, 1);
+	}
+}
